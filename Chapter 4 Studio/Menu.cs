e360958190d2eb632b18d11b3b5a581f176ae0ac
mtorres9095 <mtorres9095@gmail.com>
2020-06-05 18:58:39 -0400

    Add project files.

---
+++ Chapter 4 Studio/Menu.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace Chapter_4_Studio
+{
+    public class Menu
+    {
+        public List<MenuItem> Items { get; set; }
+        public DateTime LastUpdated { get; set; }
+
+        public Menu(List<MenuItem> items, DateTime lastUpdated)
+        {
+            Items = items;
+            LastUpdated = lastUpdated;
+        }
+
+
+    }
+}
---
+++ Chapter 4 Studio/MenuItem.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+
+namespace Chapter_4_Studio
+{
+   public class MenuItem
+    {
+        public double Price { get; set; }
+        public string Description { get; set; }
+        public string Category { get; set; }
+        public bool IsNew { get; set; }
+
+
+        public MenuItem(double price, string description, string category, bool isNew)
+        {
+            Price = price;
+            Description = description;
+            Category = category;
+            IsNew = isNew;
+        }
+
+    }
+}
